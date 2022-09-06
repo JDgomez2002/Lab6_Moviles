f02b5a564e2434317e88e7a86c91c3267b103a17
JDgomez2002 <87883400+JDgomez2002@users.noreply.github.com>
2022-09-05 22:53:07 -0600

    Navigation

---
+++ NavigationComponent/app/src/main/java/com/example/wordsapp/NavigationTests.kt
@@ -0,0 +1,32 @@
+package com.example.wordsapp
+
+import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.navigation.Navigation
+import androidx.test.core.app.ApplicationProvider
+import org.junit.runner.RunWith
+
+class NavigationTests {
+    @RunWith(AndroidJUnit4::class)
+    fun navigate_to_words_nav_component(){
+        val navController = TestNavHostController(
+            ApplicationProvider.getApplicationContext()
+        )
+
+        val letterListScenario = launchFragmentInContainer<LetterListFragment>(themeResId =
+        R.style.Theme_Words)
+
+        letterListScenario.onFragment { fragment ->
+
+            navController.setGraph(R.navigation.nav_graph)
+
+            Navigation.setViewNavController(fragment.requireView(), navController)
+        }
+
+        onView(withId(R.id.recycler_view))
+            .perform(RecyclerViewActions
+                .actionOnItemAtPosition<RecyclerView.ViewHolder>(2, click()))
+
+        assertEquals(navController.currentDestination?.id, R.id.wordListFragment)
+
+    }
+}
